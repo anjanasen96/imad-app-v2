5f435990745d66cf98a4af85d470f1bd5f95767a
anjanasen96 <anjanasen96@gmail.com>
2017-02-06 00:11:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,11 +22,11 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two',function(req,res){
-    res.send("Article two is requested. It will also be served");
+     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send("Article three is requested. It will also be served at the end");
+     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
