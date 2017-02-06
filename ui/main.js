ae0e157e83935b0b254d55a7bf625f026bdfb66f
anjanasen96 <anjanasen96@gmail.com>
2017-02-06 21:56:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,24 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
-var img = document.getElementById('madi');
+//var img = document.getElementById('madi');
 
-var marginLeft = 0;
+//var marginLeft = 0;
 
-function moveRight() {
+/* function moveRight() {
   marginLeft = marginLeft+5;
   img.style.marginLeft = marginLeft +'px';
 }
 
 img.onclick= function () {
     var interval = setInterval(moveRight,50);
+};
+*/
+
+var counter =0;
+var button = document.getElementById('counter');
+
+button.onclick= function() {
+   counter = counter + 1;
+   var span = document.getElementById('count');
+   span.innerHTML = counter.toString();
 };
