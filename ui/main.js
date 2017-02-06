18053ae0ef9198c64077a1a7d82017f6624e2050
anjanasen96 <anjanasen96@gmail.com>
2017-02-06 22:09:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,21 @@ var counter =0;
 var button = document.getElementById('counter');
 
 button.onclick= function() {
+    var request = new XMLHttprequest();
+    
+    
+    request.onReadyStateChange = function(){
+      if (request.ReadyState === XMLHttpRequest.DONE)  {
+          if (request.Status === 200){
+              counter = request.responseText;
+          }
+      }
+    };
+    /*
    counter = counter + 1;
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
+   */
+   request.open('GET','http://http://anjanasen96.imad.hasura-app.io/counter',true);
+   request.send(null);
 };
