c30b8a6c4ce6e316f3c1f5f2eac556f47792fe08
anjanasen96 <anjanasen96@gmail.com>
2017-02-07 11:18:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,6 +29,8 @@ button.onclick= function() {
          
          // Check if success
         if (request.status === 200){
+            
+            // Capture the respone and store it in a variable
               counter = request.responseText;
               var span = document.getElementById('count');
               span.innerHTML = counter.toString();
@@ -40,6 +42,8 @@ button.onclick= function() {
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
    */
+   
+   // Make a request to the counter endpoint(URL)
    request.open('GET','http://anjanasen96.imad.hasura-app.io/counter',true);
    request.send(null);
 };
