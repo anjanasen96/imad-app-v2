90e4d72e2b5833c545c0c3d5a13ef60dc86e2455
anjanasen96 <anjanasen96@gmail.com>
2017-03-05 20:26:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ img.onclick= function () {
     var interval = setInterval(moveRight,50);
 };
 */
-
+/*
 var button = document.getElementById('counter');
 var counter;
 
@@ -37,20 +37,25 @@ button.onclick= function() {
           }
       }
     };
+    */
     /*
    counter = counter + 1;
    var span = document.getElementById('count');
    span.innerHTML = counter.toString();
    */
    
-   // Make a request to the counter endpoint(URL)
-   request.open('GET','http://anjanasen96.imad.hasura-app.io/counter',true);
-   request.send(null);
-};
+   /* Make a request to the counter endpoint(URL)
+  // request.open('GET','http://anjanasen96.imad.hasura-app.io/counter',true);
+  // request.send(null);
+};  */
 
-//Submit a name
+//Submit Login credentials
 
-var nameInput = document.getElementById('name');
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+console.log(password);
+
 var submit =  document.getElementById('submit_butn');
 
 var names = [];
@@ -60,27 +65,28 @@ submit.onclick = function() {
     
     var nameValue = nameInput.value;
     
-    // Make a request to the counter endpoint(URL)
-   request.open('GET','http://anjanasen96.imad.hasura-app.io/submit-name?name='+nameValue,true);
-   request.send(null);
+    // Make a request to the login endpoint(URL)
+   
+   request.open('POST','http://anjanasen96.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username: username, password: password}));
     
     // On state change, browser will know. Detect state change.
     
-    request.onreadystatechange = function(){
+    request.onreadystatedchange = function(){
       if (request.readyState === XMLHttpRequest.DONE)  {
          
          // Check if success
         if (request.status === 200){
             
-            // Capture the respone and store it in a variable
-              names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-            for (var i =0; i <names.length; i++){
-                    list += '<li>' + names[i]+ '</li>';
-                }
-                var ul = document.getElementById('nameList');
-                ul.innerHTML = list;
+           console.log('User logged in');
+           alert('Log in is successful');
+        }
+        else if (request.status === 403){
+            alert('Username/password is incorrect');
+        }
+        else if (request.status === 500){
+            alert('Something went worng in the server');
         }
       }
     };
