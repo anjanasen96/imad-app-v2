f11d3d0a5a30f01561649da3fd80bb943d2804e7
anjanasen96 <anjanasen96@gmail.com>
2017-03-10 22:24:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,7 @@ function createTemplate (data)
                 </div>
                 <div id="comments">
                 <center>Loading comments...</center>
+                 <script type="text/javascript" src="/ui/article.js"></script>
               </div>
             </div>
           </body>
