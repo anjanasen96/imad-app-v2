9723af4e7544a267a4b2508f6613f36042aad974
anjanasen96 <anjanasen96@gmail.com>
2017-03-10 22:24:05 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -99,5 +99,8 @@ function loadComments () {
 
 
 // The first thing to do is to check if the user is logged in!
-loadLogin();
+/* loadLogin();
 loadComments();
+*/
+
+alert("I am inside article.js");
