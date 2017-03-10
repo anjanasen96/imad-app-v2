a0e3445969a2cf9aa8558c79cd1cb2eb08351977
anjanasen96 <anjanasen96@gmail.com>
2017-03-11 00:11:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -357,7 +357,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/article.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+  res.sendFile(path.join(__dirname, 'ui', 'article.js'));
 });
 
 
